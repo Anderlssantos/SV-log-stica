f9c02fcd35768a697cc7e1a8a63cbe7334e5267a
anderlssantos <andeluissantos@hotmail.com>
2024-01-14 09:46:55 -0300

    SV Logística

---
+++ script.js
@@ -0,0 +1,29 @@
+let formulario = document.querySelector(".fale-conosco")
+let sobre = document.querySelector(".desccricao")
+let section = document.querySelector(".section")
+// const origim= await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/distritos".then(response=>response.json()))
+
+
+function descricao(){}
+
+function cliqueinoBotao() {
+    formulario.style.right= "500px"
+
+}
+function voltar() {
+    formulario.style.right = "-330px"
+
+}
+function enviarFormulario() {
+    // Aqui você pode adicionar lógica para enviar os dados do formulário para o servidor
+    alert("Formulário enviado com sucesso!");
+  }
+
+
+
+
+
+
+
+
+
